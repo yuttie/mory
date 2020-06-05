9cdd7eeedf0fc9624d4cb129c12b7212e42c4c34
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-05 17:36:09 +0900

    Add a route '/list' returning a mockup response

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let api = filters::notes();
+    let routes = api;
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+mod filters {
+    use warp::Filter;
+    use log::debug;
+
+    pub fn notes(
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        notes_list()
+    }
+
+    pub fn notes_list(
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path("list")
+            .map(|| {
+                debug!("list");
+                let files = vec!["Home.md", "test.md"];
+                warp::reply::json(&files)
+            })
+    }
 }
