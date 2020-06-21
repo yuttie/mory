03849e90fe9bc7a0b60702bb290b6018edf2f2ce
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-21 22:38:30 +0900

    Add views for finding and viewing notes

---
+++ frontend/src/views/Find.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="find">
+    <ul>
+      <li
+        v-for="entry of entries"
+        v-bind:key="entry"><a v-bind:href="`/view/${entry}`">{{ entry }}</a></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+import axios from 'axios';
+
+@Component
+export default class Home extends Vue {
+  entries: string[] = [];
+
+  mounted() {
+    axios.get('http://localhost:3030/list')
+      .then(res => {
+        this.entries = res.data;
+      });
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.editor {
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  margin-left: -50%;
+  transition: margin-left 500ms;
+  resize: none;
+}
+
+.rendered {
+  margin-left: 0;
+  width: 100%;
+  height: 100%;
+  transition: margin-left 500ms,
+              width 500ms;
+}
+
+.editor.shifted {
+  margin-left: 0;
+}
+
+.rendered.shifted {
+  margin-left: 50%;
+  width: 50%;
+}
+</style>
---
+++ frontend/src/views/View.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="view">
+    <button type="button" v-on:click="toggleEditor">{{ editorIsVisible ? 'Hide editor' : 'Edit' }}</button>
+    <div>
+      <textarea v-model="text" class="editor" v-bind:class="{ shifted: editorIsVisible }"></textarea>
+      <div v-html="rendered" class="rendered" v-bind:class="{ shifted: editorIsVisible }"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+import axios from 'axios';
+import marked from 'marked';
+
+@Component
+export default class Home extends Vue {
+  text = '';
+  editorIsVisible = false;
+
+  mounted() {
+    console.log(this.$route.params.path);
+    axios.get(`http://localhost:3030/load/${this.$route.params.path}`)
+      .then(res => {
+        this.text = res.data;
+      });
+  }
+
+  get rendered() {
+    return marked(this.text);
+  }
+
+  toggleEditor() {
+    this.editorIsVisible = !this.editorIsVisible;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.editor {
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  margin-left: -50%;
+  transition: margin-left 500ms;
+  resize: none;
+}
+
+.rendered {
+  margin-left: 0;
+  width: 100%;
+  height: 100%;
+  transition: margin-left 500ms,
+              width 500ms;
+}
+
+.editor.shifted {
+  margin-left: 0;
+}
+
+.rendered.shifted {
+  margin-left: 50%;
+  width: 50%;
+}
+</style>
