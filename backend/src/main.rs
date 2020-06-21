895c320ea2bc2f9fe7153d5406f630f09f4cd7fc
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-21 19:15:21 +0900

    Add /save

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -32,7 +32,8 @@ mod filters {
         repo: Arc<Mutex<Repository>>,
     ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
         notes_list(repo.clone())
-            .or(notes_load(repo))
+            .or(notes_load(repo.clone()))
+            .or(notes_save(repo))
     }
 
     pub fn notes_list(
@@ -51,15 +52,27 @@ mod filters {
             .and(warp::any().map(move || repo.clone()))
             .and_then(handlers::load_note)
     }
+
+    pub fn notes_save(
+        repo: Arc<Mutex<Repository>>,
+    ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path("save")
+            .and(warp::post())
+            .and(warp::body::json())
+            .and(warp::any().map(move || repo.clone()))
+            .and_then(handlers::save_note)
+    }
 }
 
 mod handlers {
+    use super::models::{NoteSave};
+
     use std::sync::Arc;
     use std::convert::Infallible;
     use std::vec::Vec;
     use std::string::String;
 
-    use git2::Repository;
+    use git2::{Repository, Index, IndexEntry, IndexTime, Oid, Signature};
     use tokio::sync::{Mutex};
     use tokio;
     use log::debug;
@@ -112,5 +125,59 @@ mod handlers {
         else {
             Err(warp::reject::not_found())
         }
+    }
+
+    pub async fn save_note(note_save: NoteSave, repo: Arc<Mutex<Repository>>) -> Result<impl warp::Reply, Infallible> {
+        debug!("save");
+        let repo = repo.lock().await;
+
+        let head = repo.head().unwrap();
+        let head_tree = head.peel_to_tree().unwrap();
+        let head_commit = head.peel_to_commit().unwrap();
+
+        let mut index = Index::new().unwrap();
+        index.read_tree(&head_tree).unwrap();
+        repo.set_index(&mut index).unwrap();
+
+        let entry = IndexEntry {
+            ctime: IndexTime::new(0, 0),
+            mtime: IndexTime::new(0, 0),
+            dev: 0,
+            ino: 0,
+            mode: 0o100644,
+            uid: 0,
+            gid: 0,
+            file_size: 0,
+            id: Oid::zero(),
+            flags: 0,
+            flags_extended: 0,
+            path: note_save.path.into_bytes(),
+        };
+        index.add_frombuffer(&entry, note_save.content.as_bytes()).unwrap();
+
+        let tree_oid = index.write_tree().unwrap();
+        let tree = repo.find_tree(tree_oid).unwrap();
+
+        let signature = Signature::now("John Doe", "jd@example.com").unwrap();
+        repo.commit(
+            Some("HEAD"),
+            &signature,
+            &signature,
+            &note_save.message,
+            &tree,
+            &[&head_commit],
+        ).unwrap();
+        Ok(warp::reply::json(&true))
+    }
+}
+
+mod models {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Deserialize, Serialize, Clone)]
+    pub struct NoteSave {
+        pub path: String,
+        pub content: String,
+        pub message: String,
     }
 }
