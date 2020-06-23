ae59c61cbd433cdf7b5dcd20576ec2f22fcde2ab
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-23 22:58:42 +0900

    Add a link to create a new note

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/create">Create</router-link> |
       <router-link to="/find">Find</router-link> |
       <router-link to="/about">About</router-link>
     </div>
