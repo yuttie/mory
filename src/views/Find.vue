55790da645cb50b5b5048cce017a5400b6752f54
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-24 13:40:22 +0900

    Add filtering function to /find

--- src/views/Find.vue
+++ src/views/Find.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="find">
     <div>
-      <span
-        v-for="tag of tags"
-        v-bind:key="tag"
-        style="border: 1px solid #ccc; border-radius: 4px; margin: 0 4px;"
-      >{{ tag }}</span>
+      <input v-on:input="onQueryInput($event)" type="text" class="query" autofocus ref="query">
+      <div class="tags">
+        <span
+          v-for="tag of tags"
+          v-bind:key="tag"
+          v-bind:class="{ 'not-in-query': queryTags.size > 0 && !queryTags.has(tag) }"
+          class="tag"
+        >{{ tag }}</span>
+      </div>
     </div>
-    <ul>
+    <ul class="list">
       <li
-        v-for="entry of entries"
+        v-for="entry of matchedEntries"
         v-bind:key="entry[0]"><a v-bind:href="`/view/${entry[0]}`">{{ entry[0] }}</a></li>
     </ul>
   </div>
@@ -24,8 +28,45 @@ import axios from 'axios';
 export default class Find extends Vue {
   entries: [string, any][] = [];
   tags: string[] = [];
+  queryKeywords: Set<any> = new Set();
+  queryTags: Set<any> = new Set();
+
+  get matchedEntries() {
+    const matched = [];
+
+    const queryKeywords = [...this.queryKeywords];
+    const queryTags = [...this.queryTags];
+
+    for (const entry of this.entries) {
+      if (queryKeywords.length > 0) {
+        if (queryKeywords.some(kw => !entry[0].includes(kw))) {
+          continue;
+        }
+      }
+      if (queryTags.length > 0) {
+        if (entry[1] === null) {
+          continue;
+        }
+        else {
+          if (!Object.prototype.hasOwnProperty.call(entry[1], 'tags')) {
+            continue;
+          }
+          else {
+            if (queryTags.some(tag => !entry[1].tags.includes(tag))) {
+              continue;
+            }
+          }
+        }
+      }
+      matched.push(entry);
+    }
+
+    return matched;
+  }
 
   mounted() {
+    window.addEventListener('keydown', this.handleKeydown);
+
     axios.get('http://localhost:3030/notes')
       .then(res => {
         const tags = new Set();
@@ -42,33 +83,64 @@ export default class Find extends Vue {
         this.tags = Array.from(tags) as string[];
       });
   }
+
+  destroyed() {
+    window.removeEventListener('keydown', this.handleKeydown);
+  }
+
+  handleKeydown(e: KeyboardEvent) {
+    if (e.key === '/') {
+      (this.$refs.query as HTMLInputElement).focus();
+      e.preventDefault();
+    }
+  }
+
+  onQueryInput(e: InputEvent) {
+    const query = (e.target as HTMLInputElement).value;
+    const queryKeywords = new Set();
+    const queryTags = new Set();
+    for (const match of query.matchAll(/"([^"]+)"|([^\s]+)/g)) {
+      const text = match[1] || match[2];
+      if (text.startsWith('#')) {
+        queryTags.add(text.slice(1));
+      }
+      else {
+        queryKeywords.add(text);
+      }
+    }
+    this.queryKeywords = queryKeywords;
+    this.queryTags = queryTags;
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.editor {
-  position: absolute;
-  width: 50%;
-  height: 100%;
-  margin-left: -50%;
-  transition: margin-left 500ms;
-  resize: none;
+.find {
+  display: flex;
+  flex-direction: column;
 }
 
-.rendered {
-  margin-left: 0;
+.query {
+  display: block;
   width: 100%;
-  height: 100%;
-  transition: margin-left 500ms,
-              width 500ms;
+  font-size: 1.2em;
+  padding: 0.2em 0.4em;
 }
 
-.editor.shifted {
-  margin-left: 0;
+.list {
+  flex: 1 1 0;
+  overflow: auto;
+  margin: 0;
+  padding: 1em;
 }
 
-.rendered.shifted {
-  margin-left: 50%;
-  width: 50%;
+.tag {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin: 0 4px;
+
+  &.not-in-query {
+    opacity: 0.5;
+  }
 }
 </style>
