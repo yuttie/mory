ff0989b8584702046c0428828bee277ed0210a67
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-25 03:49:59 +0900

    Add src/custom.scss for customizing styles of rendered notes

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,13 @@ process.env.VUE_APP_NAME = require('./package.json').name;
 process.env.VUE_APP_VERSION = require('./package.json').version;
 process.env.VUE_APP_AUTHOR = require('./package.json').author.replace(/<[^>]+>/, '').trim();
 process.env.VUE_APP_BUILD_YEAR = new Date().getFullYear();
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: '@import "@/custom.scss";',
+      },
+    },
+  },
+};
