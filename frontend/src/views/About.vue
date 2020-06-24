8e4a513b5a7abb83c094795646d7b33e947d3dfc
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-24 21:39:47 +0900

    Implement /about

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="logo"><img src="@/assets/logo.png"></div>
+    <div class="name">{{ name }}</div>
+    <div class="version">Version {{ version }}</div>
+    <div class="copyright">Copyright © {{ fromYear }}&ndash;{{ buildYear }} {{ author }}</div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class About extends Vue {
+  name: string = process.env.VUE_APP_NAME;
+  version: string = process.env.VUE_APP_VERSION;
+  author: string = process.env.VUE_APP_AUTHOR;
+  fromYear = 2020;
+  buildYear: number = process.env.VUE_APP_BUILD_YEAR;
+}
+</script>
+
+<style scoped lang="scss">
+.about {
+  padding: 1em;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &::before, &::after {
+    content: '';
+    flex-grow: 1;
+  }
+}
+
+.logo {
+  padding-bottom: 1em;
+  border-bottom: 1px solid #ccc;
+}
+
+.name {
+  color: #333;
+  font-family: sans-serif;
+  font-size: 4em;
+  font-weight: normal;
+  line-height: 1.2em;
+  $letter-spacing: 0.5em;
+  padding-left: $letter-spacing;
+  letter-spacing: $letter-spacing;
+}
+
+.copyright {
+  margin-top: 2em;
+}
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,4 @@
+process.env.VUE_APP_NAME = require('./package.json').name;
+process.env.VUE_APP_VERSION = require('./package.json').version;
+process.env.VUE_APP_AUTHOR = require('./package.json').author.replace(/<[^>]+>/, '').trim();
+process.env.VUE_APP_BUILD_YEAR = new Date().getFullYear();
