18bb523e60573e5b5c03e227cab563fde0994530
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-06-29 10:46:30 +0900

    Add an icon of size 192x192

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,11 @@ module.exports = {
     manifestOptions: {
       icons: [
         {
+          'src': './img/icons/android-chrome-192x192.png',
+          'sizes': '192x192',
+          'type': 'image/png'
+        },
+        {
           'src': './img/icons/android-chrome-512x512.png',
           'sizes': '512x512',
           'type': 'image/png'
