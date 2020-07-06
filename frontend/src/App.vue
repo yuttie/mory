1c4f9f933dcf966989e18d81bbef4ce40302081f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-07-06 14:40:57 +0900

    Use 'sans-serif' as a default font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -104,11 +104,12 @@ export default class App extends Vue {
 html, body {
   padding: 0;
   margin: 0;
-}
-
-html, body {
   width: 100%;
   height: 100%;
+}
+
+html {
+  font-family: sans-serif;
 }
 </style>
 
