c5258f61fc5ffd095ad9c2a41386421cf306d7f0
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-08-13 13:16:33 +0900

    Add apple-touch-icon-152x152.png and msapplication-icon-144x144.png

--- vue.config.js
+++ vue.config.js
@@ -11,9 +11,9 @@ module.exports = {
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: null,
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
       maskIcon: null,
-      msTileImage: null,
+      msTileImage: 'img/icons/msapplication-icon-144x144.png',
     },
     manifestOptions: {
       icons: [
