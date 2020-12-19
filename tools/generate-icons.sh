47c31f279aeae9a44a2728915d2e11df8948146f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2020-12-19 21:19:45 +0900

    Change the size of apple-touch-icon

--- tools/generate-icons.sh
+++ tools/generate-icons.sh
@@ -6,7 +6,7 @@ inkscape --export-type=png --export-area-page -w 512 -h 512 -o public/favicon.pn
 # PWA icons
 inkscape --export-type=png --export-area-page -w  32 -h  32 -o public/img/icons/favicon-32x32.png logo.svg
 inkscape --export-type=png --export-area-page -w  16 -h  16 -o public/img/icons/favicon-16x16.png logo.svg
-inkscape --export-type=png --export-area-page -w 152 -h 152 -o public/img/icons/apple-touch-icon-152x152.png logo.svg
+inkscape --export-type=png --export-area-page -w 180 -h 180 -o public/img/icons/apple-touch-icon-180x180.png logo.svg
 inkscape --export-type=png --export-area-page -w 144 -h 144 -o public/img/icons/msapplication-icon-144x144.png logo.svg
 inkscape --export-type=png --export-area-page -w 192 -h 192 -o public/img/icons/android-chrome-192x192.png logo.svg
 inkscape --export-type=png --export-area-page -w 512 -h 512 -o public/img/icons/android-chrome-512x512.png logo.svg
