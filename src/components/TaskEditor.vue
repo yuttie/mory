a9bc192789971a05436d774ce45607ef5ca6a851
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-04-14 19:36:11 +0900

    Add TaskEditor component

---
+++ src/components/TaskEditor.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="task">
+    <v-text-field
+      label="Name"
+      v-model="value.name"
+    ></v-text-field>
+    <v-menu
+      v-model="deadlineMenu"
+      v-bind:close-on-content-click="true"
+      v-bind:nudge-right="40"
+      transition="scale-transition"
+      offset-y
+      min-width="auto"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="value.deadline"
+          label="Deadline"
+          prepend-icon="mdi-calendar"
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        v-model="value.deadline"
+        @input="menu2 = false"
+      ></v-date-picker>
+    </v-menu>
+    <v-menu
+      v-model="scheduleMenu"
+      v-bind:close-on-content-click="true"
+      v-bind:nudge-right="40"
+      transition="scale-transition"
+      offset-y
+      min-width="auto"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          v-model="value.schedule"
+          label="Schedule on"
+          prepend-icon="mdi-calendar"
+          readonly
+          v-bind="attrs"
+          v-on="on"
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        v-model="value.schedule"
+        @input="menu2 = false"
+      ></v-date-picker>
+    </v-menu>
+    <v-checkbox
+      label="Done"
+      v-model="value.done"
+    ></v-checkbox>
+    <v-combobox
+      v-model="value.tags"
+      v-bind:items="knownTags"
+      chips
+      clearable
+      label="Your favorite hobbies"
+      multiple
+      prepend-icon="mdi-filter-variant"
+      solo
+    >
+      <template v-slot:selection="{ attrs, item, select, selected }">
+        <v-chip
+          v-bind="attrs"
+          v-bind:input-value="selected"
+          close
+          @click="select"
+          @click:close="remove(item)"
+        >
+          <span>{{ item }}</span>
+        </v-chip>
+      </template>
+    </v-combobox>
+    <v-textarea
+      label="Note"
+      v-model="value.note"
+    ></v-textarea>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
+
+import * as api from '@/api';
+import { Task } from '@/api';
+
+@Component
+export default class TaskEditor extends Vue {
+  @Prop() readonly value!: Task;
+  @Prop(Array) readonly knownTags!: string[];
+
+  deadlineMenu = false;
+  scheduleMenu = false;
+}
+</script>
+
+<style scoped lang="scss">
+.task {
+}
+</style>
