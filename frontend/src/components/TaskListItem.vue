ba42787c3ddc7e60083a10084a4206cb7f800caa
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-06-13 21:34:48 +0900

    TaskListItem: Fix warning

--- frontend/src/components/TaskListItem.vue
+++ frontend/src/components/TaskListItem.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Watch, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
 import { Task } from '@/api';
 
