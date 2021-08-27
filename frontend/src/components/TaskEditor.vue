54b6ef8e59ff4cbd45310edabd4e75df8ec7fded
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2021-08-27 19:33:30 +0900

    TaskEditor: Improve layout

--- frontend/src/components/TaskEditor.vue
+++ frontend/src/components/TaskEditor.vue
@@ -1,105 +1,133 @@
 <template>
   <div class="task">
-    <v-text-field
-      label="Name"
-      autofocus
-      prepend-icon="mdi-pencil"
-      v-bind:value="value.name"
-      v-on:input="$emit('input', { ...value, name: $event })"
-    ></v-text-field>
-    <v-menu
-      v-model="deadlineMenu"
-      v-bind:close-on-content-click="false"
-      v-bind:nudge-right="40"
-      offset-y
-      min-width="auto"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-text-field
-          v-bind:value="value.deadline"
-          v-on:input="$emit('input', { ...value, deadline: $event })"
-          label="Deadline"
-          prepend-icon="mdi-calendar"
-          readonly
-          clearable
-          v-bind="attrs"
-          v-on="on"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-        v-bind:value="value.deadline"
-        v-on:input="$emit('input', { ...value, deadline: $event }); deadlineMenu = false;"
-      ></v-date-picker>
-    </v-menu>
-    <v-row>
-      <v-col>
-        <v-menu
-          v-model="scheduleMenu"
-          v-bind:close-on-content-click="false"
-          v-bind:nudge-right="40"
-          offset-y
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-bind:value="value.schedule"
-              v-on:input="$emit('input', { ...value, schedule: $event })"
-              label="Schedule on"
-              prepend-icon="mdi-calendar"
-              readonly
-              clearable
-              v-bind="attrs"
-              v-on="on"
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-text-field
+            label="Name"
+            autofocus
+            dense
+            prepend-icon="mdi-pencil"
+            v-bind:value="value.name"
+            v-on:input="$emit('input', { ...value, name: $event })"
             ></v-text-field>
-          </template>
-          <v-date-picker
-            v-bind:value="value.schedule"
-            v-on:input="$emit('input', { ...value, schedule: $event }); scheduleMenu = false;"
-          ></v-date-picker>
-        </v-menu>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn
-          text
-          v-on:click="setScheduleToday"
-        >Today</v-btn>
-      </v-col>
-    </v-row>
-    <v-checkbox
-      label="Done"
-      v-bind:input-value="value.done"
-      v-bind:value="value.done"
-      v-on:change="$emit('input', { ...value, done: $event })"
-    ></v-checkbox>
-    <v-combobox
-      v-bind:value="value.tags"
-      v-on:input="$emit('input', { ...value, tags: $event })"
-      v-bind:items="tagItems"
-      v-bind:return-object="false"
-      chips
-      clearable
-      label="Tags"
-      multiple
-      prepend-icon="mdi-tag-multiple-outline"
-    >
-      <template v-slot:selection="{ attrs, item, select, selected }">
-        <v-chip
-          v-bind="attrs"
-          v-bind:input-value="selected"
-          close
-          v-on:click="select"
-          v-on:click:close="removeTag(item)"
-        >
-          <span>{{ item }}</span>
-        </v-chip>
-      </template>
-    </v-combobox>
-    <v-textarea
-      label="Note"
-      v-bind:value="value.note"
-      v-on:input="$emit('input', { ...value, note: $event })"
-      prepend-icon="mdi-text"
-    ></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-combobox
+            v-bind:value="value.tags"
+            v-on:input="$emit('input', { ...value, tags: $event })"
+            v-bind:items="tagItems"
+            v-bind:return-object="false"
+            chips
+            clearable
+            dense
+            label="Tags"
+            multiple
+            prepend-icon="mdi-tag-multiple-outline"
+          >
+            <template v-slot:selection="{ attrs, item, select, selected }">
+              <v-chip
+                v-bind="attrs"
+                v-bind:input-value="selected"
+                close
+                small
+                v-on:click="select"
+                v-on:click:close="removeTag(item)"
+              >
+                <span>{{ item }}</span>
+              </v-chip>
+            </template>
+          </v-combobox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-menu
+            v-model="scheduleMenu"
+            v-bind:close-on-content-click="false"
+            v-bind:nudge-right="40"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-bind:value="value.schedule"
+                v-on:input="$emit('input', { ...value, schedule: $event })"
+                label="Schedule on"
+                prepend-icon="mdi-calendar"
+                readonly
+                clearable
+                dense
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-bind:value="value.schedule"
+              v-on:input="$emit('input', { ...value, schedule: $event }); scheduleMenu = false;"
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
+        <v-col cols="auto">
+          <v-btn
+            text
+            dense
+            v-on:click="setScheduleToday"
+          >Today</v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-menu
+            v-model="deadlineMenu"
+            v-bind:close-on-content-click="false"
+            v-bind:nudge-right="40"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-bind:value="value.deadline"
+                v-on:input="$emit('input', { ...value, deadline: $event })"
+                label="Deadline"
+                prepend-icon="mdi-calendar"
+                readonly
+                clearable
+                dense
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-bind:value="value.deadline"
+              v-on:input="$emit('input', { ...value, deadline: $event }); deadlineMenu = false;"
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
+        <v-col cols="auto">
+          <v-checkbox
+            label="Done"
+            dense
+            v-bind:input-value="value.done"
+            v-bind:value="value.done"
+            v-on:change="$emit('input', { ...value, done: $event })"
+          ></v-checkbox>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-textarea
+            label="Note"
+            dense
+            v-bind:value="value.note"
+            v-on:input="$emit('input', { ...value, note: $event })"
+            prepend-icon="mdi-text"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
