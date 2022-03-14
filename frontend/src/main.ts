e17d9a5f0df52916c4445ab2e2dd41f048179622
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-03-14 23:28:01 +0900

    Install and enable '@vue/composition-api'

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import VueCompositionAPI from '@vue/composition-api';
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false
 
