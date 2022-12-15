b69875f8df8d9601675534390349c0ed2e54d92d
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-12-16 00:32:54 +0900

    Move loadConfigValue and saveConfigValue to src/config.ts

--- frontend/src/views/Config.vue
+++ frontend/src/views/Config.vue
@@ -81,21 +81,8 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 import * as api from '@/api';
+import { loadConfigValue, saveConfigValue } from '@/config';
 import YAML from 'yaml';
-
-function loadConfigValue(key: string, default_: any): any {
-  const value = localStorage.getItem(key);
-  if (value === null) {
-    return default_;
-  }
-  else {
-    return JSON.parse(value);
-  }
-}
-
-function saveConfigValue(key: string, value: any) {
-  localStorage.setItem(key, JSON.stringify(value));
-}
 
 @Component
 export default class Config extends Vue {
