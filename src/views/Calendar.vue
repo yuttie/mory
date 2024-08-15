40962bd590ffe23e87a1c366b9140e622535fee4
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-08-15 16:00:24 +0900

    refactor(Calendar): Remove unused imports

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -94,11 +94,9 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, watch, onMounted, onUnmounted, nextTick, defineProps, defineEmits, defineExpose } from 'vue';
-import type { Ref } from 'vue';
+import { ref, computed, watch, onMounted, onUnmounted, defineEmits, defineExpose } from 'vue';
 
 import { useRouter, useRoute } from '@/composables/vue-router';
-import { useVuetify } from '@/composables/vuetify';
 
 import * as api from '@/api';
 import { isMetadataEventMultiple, validateEvent } from '@/api';
