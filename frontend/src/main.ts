9c63e450575e297b0640a03d87492aa5c92a827f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-11-26 19:00:39 +0900

    style: Add semicolons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import { createPinia, PiniaVuePlugin } from 'pinia'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia, PiniaVuePlugin } from 'pinia';
 import vuetify from './plugins/vuetify';
 
-Vue.use(PiniaVuePlugin)
-const pinia = createPinia()
+Vue.use(PiniaVuePlugin);
+const pinia = createPinia();
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   pinia,
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
