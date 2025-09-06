6252ec34b7a4986f4d785fbaf864387cb446fc05
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-09-06 15:42:58 +0900

    style(frontend): Improve contrast ratio of active item in task tree from 1.94 to 3.01 by adjustying primary color

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -19,7 +19,7 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: '#29C4CC',
+        primary: '#0099a1',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
