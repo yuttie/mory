13873e6ba14151b7ef3661f801acb23af5972958
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-09-08 20:17:12 +0900

    Use four-space indent

--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="editor">
-    <div ref="editorEl"></div>
-  </div>
+    <div class="editor">
+        <div ref="editorEl"></div>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -13,14 +13,14 @@ import ace from 'ace-builds';
 
 // Props
 const props = defineProps<{
-  value: string;
-  mode: string;
+    value: string;
+    mode: string;
 }>();
 
 // Emits
 const emit = defineEmits<{
-  (e: 'change', value: string): void;
-  (e: 'scroll', lineNumber: number): void;
+    (e: 'change', value: string): void;
+    (e: 'scroll', lineNumber: number): void;
 }>();
 
 // Reactive states
@@ -32,177 +32,177 @@ const editorEl = ref(null);
 
 // Lifecycle hooks
 onMounted(() => {
-  editor.value = ace.edit(editorEl.value as Element, {
-    fontSize: loadConfigValue('editor-font-size', 14),
-    fontFamily: loadConfigValue('editor-font-family', 'Menlo, monospace'),
-    useSoftTabs: true,
-    navigateWithinSoftTabs: true,
-    enableAutoIndent: true,
-    showGutter: true,
-    showLineNumbers: true,
-    showPrintMargin: false,
-    displayIndentGuides: true,
-    highlightActiveLine: true,
-    highlightGutterLine: true,
-    highlightSelectedWord: true,
-    showInvisibles: false,
-    wrap: true,
-    indentedSoftWrap: true,
-    scrollPastEnd: 1.0,
-    animatedScroll: false,
-  });
-  editor.value!.on('change', () => {  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    emit('change', editor.value!.getValue());  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-  });
-  editor.value!.getSession().on('changeScrollTop', (_e: any) => {  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    if (!ignoreNextChangeScrollTopEvent.value) {
-      const lineNumber = editor.value!.renderer.getFirstFullyVisibleRow();
-      emit('scroll', lineNumber);  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    }
-    else {
-      ignoreNextChangeScrollTopEvent.value = false;
-    }
-  });
+    editor.value = ace.edit(editorEl.value as Element, {
+        fontSize: loadConfigValue('editor-font-size', 14),
+        fontFamily: loadConfigValue('editor-font-family', 'Menlo, monospace'),
+        useSoftTabs: true,
+        navigateWithinSoftTabs: true,
+        enableAutoIndent: true,
+        showGutter: true,
+        showLineNumbers: true,
+        showPrintMargin: false,
+        displayIndentGuides: true,
+        highlightActiveLine: true,
+        highlightGutterLine: true,
+        highlightSelectedWord: true,
+        showInvisibles: false,
+        wrap: true,
+        indentedSoftWrap: true,
+        scrollPastEnd: 1.0,
+        animatedScroll: false,
+    });
+    editor.value!.on('change', () => {  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        emit('change', editor.value!.getValue());  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    });
+    editor.value!.getSession().on('changeScrollTop', (_e: any) => {  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        if (!ignoreNextChangeScrollTopEvent.value) {
+            const lineNumber = editor.value!.renderer.getFirstFullyVisibleRow();
+            emit('scroll', lineNumber);  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        }
+        else {
+            ignoreNextChangeScrollTopEvent.value = false;
+        }
+    });
 
-  const theme = loadConfigValue('editor-theme', 'default');
-  setTheme(theme);
+    const theme = loadConfigValue('editor-theme', 'default');
+    setTheme(theme);
 
-  const modeModules = import.meta.glob('../../node_modules/ace-builds/src-noconflict/mode-*.js');
-  const modeModulePath = `../../node_modules/ace-builds/src-noconflict/mode-${props.mode}.js`;
-  modeModules[modeModulePath]().then((_mod) => {
-    editor.value!.getSession().setMode(`ace/mode/${props.mode}`);
-  });
+    const modeModules = import.meta.glob('../../node_modules/ace-builds/src-noconflict/mode-*.js');
+    const modeModulePath = `../../node_modules/ace-builds/src-noconflict/mode-${props.mode}.js`;
+    modeModules[modeModulePath]().then((_mod) => {
+        editor.value!.getSession().setMode(`ace/mode/${props.mode}`);
+    });
 
-  const keybinding = loadConfigValue('editor-keybinding', 'default');
-  setKeybinding(keybinding);
+    const keybinding = loadConfigValue('editor-keybinding', 'default');
+    setKeybinding(keybinding);
 });
 
 // Methods
 function focus() {
-  editor.value!.focus();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    editor.value!.focus();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
 }
 
 function blur() {
-  editor.value!.blur();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    editor.value!.blur();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
 }
 
 function resize() {
-  editor.value!.resize();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+    editor.value!.resize();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
 }
 
 function scrollTo(lineNumber: number) {
-  editor.value!.scrollToLine(lineNumber, false, false, null);
-  ignoreNextChangeScrollTopEvent.value = true;
+    editor.value!.scrollToLine(lineNumber, false, false, null);
+    ignoreNextChangeScrollTopEvent.value = true;
 }
 
 function getSelection(): string {
-  const range = editor.value!.getSelectionRange();
-  return editor.value!.session.getTextRange(range);
+    const range = editor.value!.getSelectionRange();
+    return editor.value!.session.getTextRange(range);
 }
 
 function replaceSelection(newText: string) {
-  const range = editor.value!.getSelectionRange();
-  editor.value!.session.remove(range);
-  editor.value!.insert(newText);
+    const range = editor.value!.getSelectionRange();
+    editor.value!.session.remove(range);
+    editor.value!.insert(newText);
 }
 
 function setTheme(theme: string) {
-  let loading = null;
+    let loading = null;
 
-  const modules = import.meta.glob('../../node_modules/ace-builds/src-noconflict/theme-*.js');
-  const path = `../../node_modules/ace-builds/src-noconflict/theme-${theme}.js`;
-  if (Object.hasOwn(modules, path)) {
-    modules[path]().then((_mod) => {
-      editor.value!.setTheme(`ace/theme/${theme}`);
-    });
-  }
-  else {
-    editor.value!.setTheme(null);
-  }
+    const modules = import.meta.glob('../../node_modules/ace-builds/src-noconflict/theme-*.js');
+    const path = `../../node_modules/ace-builds/src-noconflict/theme-${theme}.js`;
+    if (Object.hasOwn(modules, path)) {
+        modules[path]().then((_mod) => {
+            editor.value!.setTheme(`ace/theme/${theme}`);
+        });
+    }
+    else {
+        editor.value!.setTheme(null);
+    }
 }
 
 function setKeybinding(keybinding: string) {
-  if (keybinding === 'default') {
-    editor.value!.setKeyboardHandler(null);
-  }
-  else if (keybinding === 'emacs') {
-    import('ace-builds/src-noconflict/keybinding-emacs').then(() => {
-      editor.value!.setKeyboardHandler('ace/keyboard/emacs');
-    });
-  }
-  else if (keybinding === 'sublime') {
-    import('ace-builds/src-noconflict/keybinding-sublime').then(() => {
-      editor.value!.setKeyboardHandler('ace/keyboard/sublime');
-    });
-  }
-  else if (keybinding === 'vim') {
-    import('ace-builds/src-noconflict/keybinding-vim').then(() => {
-      editor.value!.setKeyboardHandler('ace/keyboard/vim');
-    });
-  }
-  else if (keybinding === 'vim-modified') {
-    import('ace-builds/src-noconflict/keybinding-vim').then(() => {
-      const keybinding = ace.require('ace/keyboard/vim');  // NOTE: `ace.require()` does not fetch the module. We need the dynamic import for actual loading.
-      adjustKeybindings(keybinding.Vim);
-      editor.value!.setKeyboardHandler(keybinding.handler);
-    });
-  }
-  else if (keybinding === 'vscode') {
-    import('ace-builds/src-noconflict/keybinding-vscode').then(() => {
-      editor.value!.setKeyboardHandler('ace/keyboard/vscode');
-    });
-  }
+    if (keybinding === 'default') {
+        editor.value!.setKeyboardHandler(null);
+    }
+    else if (keybinding === 'emacs') {
+        import('ace-builds/src-noconflict/keybinding-emacs').then(() => {
+            editor.value!.setKeyboardHandler('ace/keyboard/emacs');
+        });
+    }
+    else if (keybinding === 'sublime') {
+        import('ace-builds/src-noconflict/keybinding-sublime').then(() => {
+            editor.value!.setKeyboardHandler('ace/keyboard/sublime');
+        });
+    }
+    else if (keybinding === 'vim') {
+        import('ace-builds/src-noconflict/keybinding-vim').then(() => {
+            editor.value!.setKeyboardHandler('ace/keyboard/vim');
+        });
+    }
+    else if (keybinding === 'vim-modified') {
+        import('ace-builds/src-noconflict/keybinding-vim').then(() => {
+            const keybinding = ace.require('ace/keyboard/vim');  // NOTE: `ace.require()` does not fetch the module. We need the dynamic import for actual loading.
+            adjustKeybindings(keybinding.Vim);
+            editor.value!.setKeyboardHandler(keybinding.handler);
+        });
+    }
+    else if (keybinding === 'vscode') {
+        import('ace-builds/src-noconflict/keybinding-vscode').then(() => {
+            editor.value!.setKeyboardHandler('ace/keyboard/vscode');
+        });
+    }
 }
 
 function adjustKeybindings(Vim: any) {
-  Vim.map("j", "gj", "normal");
-  Vim.map("k", "gk", "normal");
-  Vim.map("<C-a>", "<Home>", "insert");
-  Vim.map("<C-e>", "<End>", "insert");
-  Vim.mapCommand("<C-b>", "motion", "moveByCharacters", { forward: false }, { context: "insert" });
-  Vim.mapCommand("<C-f>", "motion", "moveByCharacters", { forward: true }, { context: "insert" });
-  Vim.map("<C-d>", "<Del>", "insert");
-  Vim.map("<C-h>", "<BS>", "insert");
+    Vim.map("j", "gj", "normal");
+    Vim.map("k", "gk", "normal");
+    Vim.map("<C-a>", "<Home>", "insert");
+    Vim.map("<C-e>", "<End>", "insert");
+    Vim.mapCommand("<C-b>", "motion", "moveByCharacters", { forward: false }, { context: "insert" });
+    Vim.mapCommand("<C-f>", "motion", "moveByCharacters", { forward: true }, { context: "insert" });
+    Vim.map("<C-d>", "<Del>", "insert");
+    Vim.map("<C-h>", "<BS>", "insert");
 }
 
 // Watchers
 watch(() => props.value, (value: string) => {
-  if (editor.value === null) {
-    throw new Error('Editor has not been created yet.');
-  }
-
-  if (value !== editor.value.getValue()) {  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    const cursor = editor.value.getCursorPosition();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    editor.value.session.setValue(value);  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-    editor.value.moveCursorToPosition(cursor);  // eslint-disable-line @typescript-eslint/no-non-null-assertion
-
-    // Fold metadata
-    const MARKER = '---';
-    if (value.startsWith(MARKER + '\n')) {
-      const lines = value.split('\n');
-      let endLine = null;
-      for (let i = 1; i < lines.length; ++i) {
-        if (lines[i] === MARKER) {
-          endLine = i;
-          break;
-        }
-      }
-      if (endLine !== null) {
-        editor.value.getSession().addFold(
-          MARKER,
-          new ace.Range(0, MARKER.length, endLine, Infinity),
-        );
-      }
+    if (editor.value === null) {
+        throw new Error('Editor has not been created yet.');
     }
-  }
+
+    if (value !== editor.value.getValue()) {  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        const cursor = editor.value.getCursorPosition();  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        editor.value.session.setValue(value);  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        editor.value.moveCursorToPosition(cursor);  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+
+        // Fold metadata
+        const MARKER = '---';
+        if (value.startsWith(MARKER + '\n')) {
+            const lines = value.split('\n');
+            let endLine = null;
+            for (let i = 1; i < lines.length; ++i) {
+                if (lines[i] === MARKER) {
+                    endLine = i;
+                    break;
+                }
+            }
+            if (endLine !== null) {
+                editor.value.getSession().addFold(
+                    MARKER,
+                    new ace.Range(0, MARKER.length, endLine, Infinity),
+                );
+            }
+        }
+    }
 });
 
 watch(() => props.mode, (mode: string) => {
-  const modeModules = import.meta.glob('../../node_modules/ace-builds/src-noconflict/mode-*.js');
-  const path = `../../node_modules/ace-builds/src-noconflict/mode-${mode}.js`;
-  modeModules[path]().then((_mod) => {
-    editor.value!.getSession().setMode(`ace/mode/${mode}`);
-  });
+    const modeModules = import.meta.glob('../../node_modules/ace-builds/src-noconflict/mode-*.js');
+    const path = `../../node_modules/ace-builds/src-noconflict/mode-${mode}.js`;
+    modeModules[path]().then((_mod) => {
+        editor.value!.getSession().setMode(`ace/mode/${mode}`);
+    });
 });
 
 defineExpose({
@@ -217,11 +217,11 @@ defineExpose({
 
 <style scoped lang="scss">
 .editor {
-  position: relative;
-  display: flex;
+    position: relative;
+    display: flex;
 
-  & > * {
-    flex: 1 1 0;
-  }
+    & > * {
+        flex: 1 1 0;
+    }
 }
 </style>
