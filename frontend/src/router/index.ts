2fc710a0d4f9a28c79b4026405d6bb158a7d734a
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-09-10 07:26:15 +0900

    refactor(frontend/router): Use four-space indent

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -17,108 +17,108 @@ const About     = () => import('../views/About.vue');
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/calendar',
-    name: 'Calendar',
-    redirect: async (to) => {
-      const { default: dayjs } = await import('dayjs');
-      const today = dayjs();
-      return {
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/calendar',
+        name: 'Calendar',
+        redirect: async (to) => {
+            const { default: dayjs } = await import('dayjs');
+            const today = dayjs();
+            return {
+                name: 'CalendarWithDate',
+                params: { type: 'month', date: [today.format('YYYY'), today.format('MM'), today.format('DD')] },
+            };
+        },
+    },
+    {
+        path: '/calendar/:type/:date+',
         name: 'CalendarWithDate',
-        params: { type: 'month', date: [today.format('YYYY'), today.format('MM'), today.format('DD')] },
-      };
+        component: Calendar,
     },
-  },
-  {
-    path: '/calendar/:type/:date+',
-    name: 'CalendarWithDate',
-    component: Calendar,
-  },
-  {
-    path: '/tasks',
-    name: 'Tasks',
-    component: Tasks,
-  },
-  {
-    path: '/tasks-next',
-    name: 'TasksNext',
-    redirect: to => {
-      return {
+    {
+        path: '/tasks',
+        name: 'Tasks',
+        component: Tasks,
+    },
+    {
+        path: '/tasks-next',
+        name: 'TasksNext',
+        redirect: to => {
+            return {
+                name: 'TasksNextWithParams',
+                params: { selectedNodeId: '_', tab: 'descendants', viewMode: 'status' },
+            };
+        },
+    },
+    {
+        path: '/tasks-next/:selectedNodeId/:tab/:viewMode',
         name: 'TasksNextWithParams',
-        params: { selectedNodeId: '_', tab: 'descendants', viewMode: 'status' },
-      };
+        component: TasksNext,
     },
-  },
-  {
-    path: '/tasks-next/:selectedNodeId/:tab/:viewMode',
-    name: 'TasksNextWithParams',
-    component: TasksNext,
-  },
-  {
-    path: '/create',
-    name: 'Create',
-    redirect: to => {
-      return {
+    {
+        path: '/create',
+        name: 'Create',
+        redirect: to => {
+            return {
+                name: 'Note',
+                params: { path: crypto.randomUUID() + '.md' },
+                query: { mode: 'create', template: to.query.from },
+            };
+        },
+    },
+    {
+        path: '/files',
+        name: 'Files',
+        component: Files,
+    },
+    {
+        path: '/search',
+        name: 'Search',
+        component: Search,
+    },
+    {
+        path: '/note/:path*',
         name: 'Note',
-        params: { path: crypto.randomUUID() + '.md' },
-        query: { mode: 'create', template: to.query.from },
-      };
+        component: Note,
     },
-  },
-  {
-    path: '/files',
-    name: 'Files',
-    component: Files,
-  },
-  {
-    path: '/search',
-    name: 'Search',
-    component: Search,
-  },
-  {
-    path: '/note/:path*',
-    name: 'Note',
-    component: Note,
-  },
-  {
-    path: '/media/:path*',
-    name: 'Media',
-    component: Media,
-  },
-  {
-    path: '/config',
-    name: 'Config',
-    component: Config,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: About,
-  },
+    {
+        path: '/media/:path*',
+        name: 'Media',
+        component: Media,
+    },
+    {
+        path: '/config',
+        name: 'Config',
+        component: Config,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: About,
+    },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
-  routes,
-  scrollBehavior(to, _from, savedPosition) {
-    if (to.hash) {
-      return { selector: decodeURIComponent(to.hash) };
-    }
-    else if (savedPosition) {
-      return savedPosition;
-    } else {
-      return { x: 0, y: 0 };
-    }
-  },
+    mode: 'history',
+    base: import.meta.env.BASE_URL,
+    routes,
+    scrollBehavior(to, _from, savedPosition) {
+        if (to.hash) {
+            return { selector: decodeURIComponent(to.hash) };
+        }
+        else if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { x: 0, y: 0 };
+        }
+    },
 });
 
 export default router;
