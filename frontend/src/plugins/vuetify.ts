bc871d72b36131089e0eceb087ba714938d6f4e6
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2025-09-11 16:28:47 +0900

    style(frontend): Change primary color to dark color and accent color to previous primary color
    I followed the recent color design, used among web apps like ChatGPT, X,
    YouTube, that uses less colors and black-like color for active elements.

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -19,9 +19,9 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: '#0099a1',
+        primary: '#1f2626',
         secondary: '#424242',
-        accent: '#82B1FF',
+        accent: '#0099a1',
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
